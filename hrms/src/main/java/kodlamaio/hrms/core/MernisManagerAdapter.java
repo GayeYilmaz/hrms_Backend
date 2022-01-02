399bc0cb1123b5247195bca3253a3da89a50b1cc
Gaye Yılmaz <36278457+GayeYilmaz@users.noreply.github.com>
2022-01-02 18:59:57 +0300

    Mernis added(Course 7.3)

--- hrms/src/main/java/kodlamaio/hrms/core/MernisManagerAdapter.java
+++ hrms/src/main/java/kodlamaio/hrms/core/MernisManagerAdapter.java
@@ -1,14 +1,30 @@
 package kodlamaio.hrms.core;
 
+import java.rmi.RemoteException;
+
 import org.springframework.stereotype.Service;
+
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 
 @Service
 public class MernisManagerAdapter implements VerificationService{
 
 	@Override
 	public boolean verify(String firstName, String lastName, String idendityNumber, int birtYear) {
-		System.out.println(firstName +" "+lastName+" can register!");
-		return true;
+		boolean result=true;
+		KPSPublicSoapProxy kpsPublic =  new  KPSPublicSoapProxy();
+		
+		try {
+			 result = kpsPublic.TCKimlikNoDogrula(Long.parseLong(idendityNumber), firstName,lastName, birtYear);
+		} catch (NumberFormatException | RemoteException e) {
+			// TODO Auto-generated catch block
+			result=false;
+			e.printStackTrace();
+			
+		}
+		System.out.println("Doğrulama : "+(result ? "Başarılı Giriş":"Başarısız Giriş"));
+		return result;
+		
 	}
 
 
--- hrms/src/main/java/kodlamaio/hrms/core/VerificationService.java
+++ hrms/src/main/java/kodlamaio/hrms/core/VerificationService.java
@@ -1,6 +1,6 @@
 package kodlamaio.hrms.core;
 
-
+import java.rmi.RemoteException;
 
 public interface VerificationService {
 	boolean verify(String firstName,String lastName,String idendityNumber,int birtYear);
